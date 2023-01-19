fdc9140377cb87795f2ede507525b60ff268455d
Aditya Alok <alok@termux.dev>
2023-01-20 03:02:12 +0530

    test

--- build-package.sh
+++ build-package.sh
@@ -252,10 +252,6 @@ source "$TERMUX_SCRIPTDIR/scripts/build/configure/termux_step_configure_cmake.sh
 # shellcheck source=scripts/build/configure/termux_step_configure_meson.sh
 source "$TERMUX_SCRIPTDIR/scripts/build/configure/termux_step_configure_meson.sh"
 
-# Setup configure args and run haskell build system. This function is called from termux_step_configure.
-# shellcheck source=scripts/build/configure/termux_step_configure_haskell_build.sh
-source "$TERMUX_SCRIPTDIR/scripts/build/configure/termux_step_configure_haskell_build.sh"
-
 # Configure the package
 # shellcheck source=scripts/build/configure/termux_step_configure.sh
 source "$TERMUX_SCRIPTDIR/scripts/build/configure/termux_step_configure.sh"
