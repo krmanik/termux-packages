36e9fc738a53a21437e01ba3fd522d809a1567a8
Aditya Alok <alok@termux.dev>
2023-01-22 21:39:20 +0530

    cabal-install: Add patch for resolv (dependency)
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/cabal-install/build.sh
+++ packages/cabal-install/build.sh
@@ -1,3 +1,4 @@
+# shellcheck shell=bash
 TERMUX_PKG_HOMEPAGE="https://www.haskell.org/cabal/"
 TERMUX_PKG_DESCRIPTION="The command-line interface for Haskell-Cabal and Hackage"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
@@ -8,3 +9,17 @@ TERMUX_PKG_SHA256=61ce436f2e14e12bf07ea1c81402362f46275014cd841a76566f0766d0ea67
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_SUGGESTS="ghc"
 TERMUX_PKG_BUILD_DEPENDS="ghc-libs"
+
+termux_step_post_get_source() {
+	local resolv_version=0.1.2.0
+	local tmpdir="$TERMUX_PKG_TMPDIR/resolv-$resolv_version"
+
+	cabal fetch reslov-"$resolv_version" -d "$TERMUX_PKG_TMPDIR"
+
+	(
+		cd "$tmpdir"
+		patch -p1 <"$TERMUX_PKG_BUILDER_DIR"/resolv.diff
+		cabal install --lib
+	)
+
+}
